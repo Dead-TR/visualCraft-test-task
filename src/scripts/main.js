65bfeed2c1f8f8f1d1f5cbe91ee34481ee777db9
DTR <t.terry.rice@gmail.com>
2020-07-21 17:41:39 +0300

    navigation desktop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const input = document.querySelector('.roof__search-item');
+const checker = document.querySelector('#search-activator');
+
+input.addEventListener('focus', () => {
+  checker.checked = true;
+});
+
+input.addEventListener('blur', () => {
+  checker.checked = false;
+});
